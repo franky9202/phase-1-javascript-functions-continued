d6bd6bf0cdaf37026b4e78e02618dbb10469d6bb
Edgar <franky9202@gmail.com>
2022-03-27 16:35:31 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const roller = []
+function saturdayFun(roller = "roller-skate"){
+    return `This Saturday, I want to ${roller}!`
+}
+saturdayFun("bathe my dog")
+
+const office = []
+function mondayWork(office = "go to the office"){
+    return `This Monday, I will ${office}.`
+}
+mondayWork("work from home")
+
+function wrapAdjective(para = ["*"]){
+    const innerFunction = function (msg = "a hard worker"){
+        return `You are ${para}${msg}${para}!`
+    }
+    return innerFunction
+}
